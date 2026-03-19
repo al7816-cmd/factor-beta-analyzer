2fc9a5ef3bb0a01a7e6c25eaecdf015a3f32fea8
al7816-cmd <al7816@stern.nyu.edu>
2026-03-19 16:49:23 -0400

    Fix index formatting for factors DataFrame

--- risk_analyzer.py
+++ risk_analyzer.py
@@ -98,7 +98,7 @@ def run_analysis(tickers):
 
     # get factor data
     factors = load_factors()
-    factors.index = factors.index.dt.strftime('%Y-%m-%d')
+    factors.index = factors.index.strftime('%Y-%m-%d')
 
     # get ticker data
     tick = load_returns(tickers)
